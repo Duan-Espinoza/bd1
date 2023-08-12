cb9b7e2e7bc58d3b7487f9f7699d2774f9603064
Duan Antonio Espinoza Olivares <75913012+Duan-Espinoza@users.noreply.github.com>
2023-08-12 03:59:18 -0600

    update codigo

---
+++ tareas/weather-app/src/main/java/tec/bd/cli/MainCommand.java
@@ -0,0 +1,21 @@
+package tec.bd.cli;
+
+import picocli.CommandLine.Command;
+import picocli.CommandLine.HelpCommand;
+
+@Command(
+        name = "WeatherReport",
+        subcommands = {
+                WeatherByCityCommand.class,
+                WeatherByZipCodeCommand.class,
+                GUICommand.class,
+                HelpCommand.class
+        },
+        description = "Weather forecasts for cities around the world")
+public class MainCommand implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
