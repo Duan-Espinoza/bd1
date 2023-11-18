809efc8640b22364055d2c637ef1ed121a63314b
Duan Antonio Espinoza Olivares <75913012+Duan-Espinoza@users.noreply.github.com>
2023-11-17 21:15:47 -0600

    Update App.java

--- proyectos/proyecto-2/weather-app/src/main/java/tec/bd/weather/App.java
+++ proyectos/proyecto-2/weather-app/src/main/java/tec/bd/weather/App.java
@@ -3,23 +3,15 @@ package tec.bd.weather;
 import picocli.CommandLine;
 import tec.bd.weather.cli.MainCommand;
 
-//Duan Espinoza
-/**
- * VER 1.0.3!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
+
         CommandLine cmd = new CommandLine(new MainCommand());
-        cmd.setExecutionStrategy(new CommandLine.RunAll());
+        cmd.setExecutionStrategy(new CommandLine.RunAll()); // default is RunLast
         cmd.execute(args);
 
-        if (args.length == 0){
+        if (args.length == 0) {
             cmd.usage(System.out);
         }
-
-
     }
 }
